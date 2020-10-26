07262a75c111ffe147d69222335a3b727928f199
sebastiankulla <sebastiankulla90@gmail.com>
2020-10-26 12:44:51 +0100

    complex categorization is now available

--- SankeyPy/Account.py
+++ SankeyPy/Account.py
@@ -15,16 +15,17 @@ class Account:  # Todo make it possible to merge multiple accounts (checking acc
     def load_revenue(self, revenue_file):
         raise NotImplementedError
 
-    def categorize(self, category_json_file): # Todo make it possible to difference the columns booking_text and client for better categorization
+    def categorize(self, category_json_file):
         with open(category_json_file, 'r') as json_file:
             categories = json.load(json_file)
         self.revenue.loc[self.revenue.turnover > 0, 'category'] = 'Other Income'
         self.revenue.loc[self.revenue.turnover < 0, 'category'] = 'Other Expenses'
 
         for category, keyword_list in categories.items():
-            for keyword in keyword_list:
-                self.revenue.loc[
-                    self.revenue['booking_text'].str.lower().str.contains(keyword.lower()), 'category'] = category
+            for keyword_dict in keyword_list:
+                for key, value in keyword_dict.items():
+                    self.revenue.loc[
+                        self.revenue[key].str.lower().str.contains(value.lower()), 'category'] = category
 
     def get_grouped_cashflow_period(self, start_date, stop_date, avg_values=True):
         period_df = self.revenue[(self.revenue.booking_date > start_date) & (self.revenue.booking_date < stop_date)]
@@ -55,10 +56,16 @@ class ComdirectAccount(Account):
                   'names': names, 'engine': 'python', 'converters': converters}
         revenue_file = revenue_file.decode('cp1252')
         self.revenue = pd.read_csv(io.StringIO(revenue_file), **kwargs)
-        self.revenue['client'] = [result[0] if len(result) > 0 else "" for result in self.revenue['booking_text'].str.findall('(?<=Auftraggeber: ).+(?= Buchungstext:)') ]
+        self.revenue['client'] = [result[0] if len(result) > 0 else "" for result in self.revenue['booking_text'].str.findall('(?<=Auftraggeber: ).+(?= Buchungstext:)')]
         self.revenue['ref_number'] = [result[0] if len(result) > 0 else "" for result in
                                   self.revenue['booking_text'].str.findall('(?<=Ref. ).+')]
-        self.revenue['booking_text'] = [result[0] if len(result) > 0 else "" for result in self.revenue['booking_text'].str.findall('(?<=Buchungstext: ).+(?= Ref.)') ]
+        self.revenue['recipient'] = [result[0] if len(result) > 0 else "" for result in self.revenue['booking_text'].str.findall('(?<=Empfänger: ).+(?=Kto\/IBAN:)')]
+        self.revenue['iban'] = [result[0] if len(result) > 0 else "" for result in self.revenue['booking_text'].str.findall('(?<=Kto\/IBAN: ).+(?= BLZ\/BIC:)')]
+        self.revenue['bic'] = [result[0] if len(result) > 0 else "" for result in
+                                self.revenue['booking_text'].str.findall('(?<=BLZ\/BIC: ).+(?=  Buchungstext:)')]
+        self.revenue['booking_text'] = [result[0] if len(result) > 0 else "" for result in
+                                        self.revenue['booking_text'].str.findall('(?<=Buchungstext: ).+(?= Ref.)')]
+        self.revenue.to_excel("test.xlsx")
 
 
 class INGAccount(Account): #  Todo add load_revenue method for INGAccount
